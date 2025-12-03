e5fd974b1ce68bec162f83207b9747135644beac
R.C. Miessler <rmiessle@gettysburg.edu>
2025-12-03 13:29:27 -0500

    Create install.R

---
+++ install.R
@@ -0,0 +1,12 @@
+install.packages("tidyverse")
+install.packages("ggplot2")
+install.packages("tidytext")
+install.packages("readr")
+install.packages("dplyr")
+install.packages("stringr")
+install.packages("lubridate")
+
+install.packages("syuzhet")
+
+install.packages("wordcloud")
+install.packages("RColorBrewer")
